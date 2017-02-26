4f8cf71fcc2043ddd5d8d396d0d0f722d151129a
Lingyi Hu <lingyihuu@gmail.com>
2017-02-26 19:04:26 +0000

    refactor bagraph into base class

--- networks.py
+++ networks.py
@@ -2,48 +2,39 @@ import networkx as nx
 import random
 import pickle as pk
 
-class BAGraph(nx.Graph):
+# this should not be instantiated directly
+class BaseBAGraph(nx.Graph):
 
-    def __init__(self, m, N):
-        if m < 1 or m >= N:
-            raise nx.NetworkXError("Barabási–Albert network must have m >= 1"
-                                   " and m < n, m = %d, n = %d" % (m, N))
-        # start with m initial nodes
-        super().__init__(data=nx.empty_graph(m))
+    prefix = 'base'
+
+    def __init__(self, m, N, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.m = m
         self.N = N
-        self.targets = self.nodes()
-        self.repeated_nodes = []
-        self.increment()
+        self.targets = []
 
-    def set_targets(self, m):
-        # pick uniformly from repeated_nodes (preferential attachment
-        self.targets = BAGraph._random_subset(self.repeated_nodes, m)
+    def _set_targets(self, current):
+        pass
 
     def increment(self):
         current = self.m
         while current < self.N:
             self.add_edges_from(zip([current] * self.m, self.targets))
             # Add one node to the list for each new edge just created.
-            self.repeated_nodes.extend(self.targets)
-            # And the new node "current" has m edges to add to the list.
-            self.repeated_nodes.extend([current] * self.m)
-            # Now choose m unique nodes from the existing nodes
-            self.set_targets(self.m)
+            self._set_targets(current)
             current += 1
 
     def save_to_file(self, name=None):
         if not name:
-            fn = 'data/phase1/ba_{0}_m{1}.pkl'.format(str(self.number_of_nodes()), str(self.m))
-        else:
-            fn = 'data/phase1/{0}_{1}_m{2}.pkl'.format(str(name), str(self.number_of_nodes()), str(self.m))
+            name = __class__.prefix
+        fn = 'data/phase1/{0}_{1}_m{2}.pkl'.format(str(name), str(self.number_of_nodes()), str(self.m))
         with open(fn, 'wb') as file:
             pk.dump(self, file)
 
-    @staticmethod
-    def load_from_file(N, m, name=None):
+    @classmethod
+    def load_from_file(cls, N, m, name=None):
         if not name:
-            name = 'ba'
+            name = cls.prefix
         fn = 'data/phase1/{0}_{1}_m{2}.pkl'.format(str(name), str(N), str(m))
         with open(fn, 'rb') as file:
             return pk.load(file)
@@ -61,25 +52,36 @@ class BAGraph(nx.Graph):
             targets.add(x)
         return targets
 
-class RandomAttachmentGraph(BAGraph):
+class BAGraph(BaseBAGraph):
+    prefix = 'ba'
+
+    def __init__(self, m, N):
+        if m < 1 or m >= N:
+            raise nx.NetworkXError("Barabási–Albert network must have m >= 1"
+                                   " and m < n, m = %d, n = %d" % (m, N))
+        # start with m initial nodes
+        super().__init__(m, N, data=nx.empty_graph(m))
+        self.targets = self.nodes()
+        self.repeated_nodes = []
+        self.increment()
+
+    def _set_targets(self, current):
+        m = self.m
+        self.repeated_nodes.extend(self.targets)
+        # And the new node "current" has m edges to add to the list.
+        self.repeated_nodes.extend([current] * self.m)
+        # Now choose m unique nodes from the existing nodes
+        # pick uniformly from repeated_nodes (preferential attachment)
+        self.targets = BAGraph._random_subset(self.repeated_nodes, m)
+
+
+class RAGraph(BaseBAGraph):
+
+    prefix = 'ra'
+
     def __init__(self, m, N):
         super().__init__(m, N)
+        self.targets = self.nodes()
 
-    def set_targets(self):
-        return
-
-    def save_to_file(self, name=None):
-        if not name:
-            fn = 'data/phase1/ra_{0}_m{1}.pkl'.format(str(self.number_of_nodes()), str(self.m))
-        else:
-            fn = 'data/phase1/{0}_{1}_m{2}.pkl'.format(str(name), str(self.number_of_nodes()), str(self.m))
-        with open(fn, 'wb') as file:
-            pk.dump(self, file)
-
-    @staticmethod
-    def load_from_file(N, m, name=None):
-        if not name:
-            name = 'ra'
-        fn = 'data/phase1/{0}_{1}_m{2}.pkl'.format(str(name), str(N), str(m))
-        with open(fn, 'rb') as file:
-            return pk.load(file)
+    def _set_targets(self):
+        self.targets = random.sample(self.nodes(), self.m)
