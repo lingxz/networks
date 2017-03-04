32d23cc737e1e5876b50a3c5ba9652f482868287
Lingyi Hu <lingyihuu@gmail.com>
2017-03-04 01:01:03 +0000

    WIP: first task

--- networks.py
+++ networks.py
@@ -1,17 +1,21 @@
 import networkx as nx
 import random
 import pickle as pk
+import json
+import os
 
 # this should not be instantiated directly
 class BaseBAGraph(nx.Graph):
 
     prefix = 'base'
 
-    def __init__(self, m, N, *args, **kwargs):
+    def __init__(self, m, N, seed=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.m = m
         self.N = N
         self.targets = []
+        if seed is not None:
+            random.seed(seed)
 
     def _set_targets(self, current):
         pass
@@ -24,12 +28,43 @@ class BaseBAGraph(nx.Graph):
             self._set_targets(current)
             current += 1
 
-    def save_to_file(self, name=None):
+    def save_degrees(self, folder=None, root_folder=None, name=None):
         if not name:
-            name = __class__.prefix
+            name = self.prefix
+        if not root_folder:
+            root_folder = self.prefix
+        if not folder:
+            folder = "degree_distribution"
+        folder_path = "data/{0}/{1}".format(root_folder, folder)
+        os.makedirs(folder_path, exist_ok=True)
+        fn = folder_path + '/{0}_{1}_m{2}.json'.format(str(name), str(self.number_of_nodes()), str(self.m))
+        with open(fn, 'w') as f:
+            json.dump(self.degree(), f)
+
+
+    @classmethod
+    def load_degrees(cls, N, m, folder=None, root_folder=None, name=None):
+        if not name:
+            name = cls.prefix
+        if not root_folder:
+            root_folder = cls.prefix
+        if not folder:
+            folder = "degree_distribution"
+        folder_path = "data/{0}/{1}".format(root_folder, folder)
+        fn = folder_path + '/{0}_{1}_m{2}.json'.format(str(name), str(N), str(m))
+        with open(fn, 'r') as f:
+            return json.load(f)
+
+    def save_to_file(self, name=None, folder=None):
+        if not name:
+            name = self.prefix
+        if not folder:
+            folder = "phase1"
+        folder_path = "data/" + folder
+        os.makedirs(folder_path, exist_ok=True)
         fn = 'data/phase1/{0}_{1}_m{2}.pkl'.format(str(name), str(self.number_of_nodes()), str(self.m))
         with open(fn, 'wb') as file:
-            pk.dump(self, file)
+            pk.dump(self, file, protocol=pk.HIGHEST_PROTOCOL)
 
     @classmethod
     def load_from_file(cls, N, m, name=None):
@@ -52,15 +87,16 @@ class BaseBAGraph(nx.Graph):
             targets.add(x)
         return targets
 
+
 class BAGraph(BaseBAGraph):
     prefix = 'ba'
 
-    def __init__(self, m, N):
+    def __init__(self, m, N, seed=None):
         if m < 1 or m >= N:
             raise nx.NetworkXError("Barabási–Albert network must have m >= 1"
                                    " and m < n, m = %d, n = %d" % (m, N))
         # start with m initial nodes
-        super().__init__(m, N, data=nx.empty_graph(m))
+        super().__init__(m, N, seed=seed, data=nx.empty_graph(m))
         self.targets = self.nodes()
         self.repeated_nodes = []
         self.increment()
